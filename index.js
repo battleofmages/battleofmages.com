2c60be7bbe2f3757b0f0f78883316124ccd04591
Eduard Urbach <e.urbach@gmail.com>
2015-04-28 00:10:35 +0900

    Added test API for determining the country

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var aero = require("aero");
 
 aero.start();
+
+aero.app.param("ip", function(request, response, next, ip) {
+	request.country = "de";
+	next();
+});
+
+aero.app.get("/country/:ip", function(request, response) {
+	response.end(request.country);
+});
