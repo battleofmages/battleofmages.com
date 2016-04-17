c7d2ef89c77d501afb6ad6f35562a1f542b7823e
Eduard Urbach <e.urbach@gmail.com>
2016-04-18 01:00:50 +0900

    Awesome particles

--- scripts/particles.js
+++ scripts/particles.js
@@ -11,30 +11,56 @@ var particleSystem = new THREE.GPUParticleSystem({
 })
 var options = {
 	position: new THREE.Vector3(),
-	positionRandomness: .3,
+	positionRandomness: 0.3,
 	velocity: new THREE.Vector3(),
-	velocityRandomness: .5,
-	color: 0xaa88ff,
-	colorRandomness: .2,
-	turbulence: .5,
+	velocityRandomness: 1,
+	color: 0x80ff80, //0xaa88ff,
+	colorRandomness: 0.2,
+	turbulence: 0.5,
 	lifetime: 2,
-	size: 5,
+	size: 2,
 	sizeRandomness: 1
 }
 var spawnerOptions = {
-	spawnRate: 15000,
-	horizontalSpeed: 1.5,
-	verticalSpeed: 1.33,
+	spawnRate: 500,
 	timeScale: 1
 }
 
 var cursor = new THREE.Vector2()
 var cursor3d = new THREE.Vector3()
+var mouseDown = false
 var projectVector = new THREE.Vector3()
+
+/*document.onmousedown = function() {
+	mouseDown = true
+}
+
+document.onmouseup = function() {
+	mouseDown = false
+}
 
 document.onmousemove = function(e) {
     cursor.x = e.clientX
     cursor.y = e.clientY
+
+	projectVector.set(
+		(cursor.x / window.innerWidth) * 2 - 1,
+	    -(cursor.y / window.innerHeight) * 2 + 1,
+	    0.5
+	)
+
+	projectVector.unproject(camera)
+
+	let dir = projectVector.sub(camera.position).normalize()
+	let distance = - camera.position.z / dir.z
+	cursor3d = camera.position.clone().add(dir.multiplyScalar(distance))
+}*/
+
+function repositionCursor() {
+	let element = document.querySelector('.active')
+
+	cursor.x = element.offsetLeft + element.offsetWidth / 2
+    cursor.y = element.offsetTop + element.offsetHeight / 2
 
 	projectVector.set(
 		(cursor.x / window.innerWidth) * 2 - 1,
@@ -69,27 +95,31 @@ function onWindowResize() {
 	renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-function spawnParticles() {
-	var delta = clock.getDelta() * spawnerOptions.timeScale
+function animate() {
+	requestAnimationFrame(animate)
+	repositionCursor()
+
+	let delta = clock.getDelta() * spawnerOptions.timeScale
 	tick += delta
 
 	if(tick < 0)
 		tick = 0
 
 	if(delta > 0) {
-		options.position = cursor3d
-
-		for(var x = 0; x < spawnerOptions.spawnRate * delta; x++) {
-			particleSystem.spawnParticle(options)
+		for(let x = 0; x < spawnerOptions.spawnRate * delta; x++) {
+			for(let degree = 0; degree < 360; degree++) {
+				options.position = cursor3d.clone()
+				options.position.x += Math.cos(degree) * 3.5
+				options.position.y += Math.sin(degree) * 3.5
+				// options.velocity.x = Math.cos(degree)
+				// options.velocity.y = Math.sin(degree)
+				particleSystem.spawnParticle(options)
+			}
 		}
 	}
 
 	particleSystem.update(tick)
-}
 
-function animate() {
-	requestAnimationFrame(animate)
-	spawnParticles()
 	renderer.render(scene, camera)
 }
 
