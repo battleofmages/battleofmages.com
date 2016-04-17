524f065bfdc283b667c80f64dee786ee161f4656
Eduard Urbach <e.urbach@gmail.com>
2016-04-17 22:49:19 +0900

    Website prototype

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var aero = require("aero");
+let aero = require('aero')
+let app = aero()
 
-aero.start();
-
-aero.app.param("ip", function(request, response, next, ip) {
-	request.country = "de";
-	next();
-});
-
-aero.app.get("/api/country/:ip", function(request, response) {
-	response.end(request.country);
-});
+app.run()
